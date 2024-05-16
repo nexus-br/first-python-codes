c754b3d253148edaf6e78bc7e831f3a329d1c354
rogerio <161358585+nexus-br@users.noreply.github.com>
2024-05-16 13:03:30 -0300

    adds files via upload
    testing regression in python

---
+++ regressao.py
@@ -0,0 +1,6 @@
+import numpy as np
+import mathplotlib.pyplot as plt
+dias = [2,3,4,5,7,8,9,10,12,13,14,15,16,17,18,20,21,22,23]
+panca =[107,103,104,104,107,104,105,102,102,104,102,104,103,104,101,101,103,103]
+plt.scatter(dias,panca)
+plt.show()
